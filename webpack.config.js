50d91c84f82babea65046b2a2b2a59c4126197d9
zhaoxin <954650516@qq.com>
2018-03-09 00:12:50 +0800

    修正配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
+const webpack = require("webpack");
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const openBrowserWebpackPlugin = require("open-browser-webpack-plugin");
 
 const config = {
-    entry : './src/main.js',
+    entry : [ './src/main.js'],
     output : {
         path : path.resolve(__dirname, 'build'),
         filename : 'bundle.js'
     },
     module : {
         rules : [
-            {test : /.js$/, use : ['babel-loader']}
+            {
+                test : /\.js$/,
+                use : ['babel-loader'],
+                include : path.resolve(__dirname, "./src"),
+                exclude : /node_modules/
+                
+            }
         ]
     },
     plugins : [
@@ -18,7 +25,7 @@ const config = {
             template : './index.html'
         }),
         new openBrowserWebpackPlugin({
-            url : "http://localhost:3000"
+            url : "http://localhost:8080"
         })
     ]
 };
